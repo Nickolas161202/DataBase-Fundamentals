861e20d752c33edc7e47170114c36dd1152e6f67
IgorHAndreis <igorhenriqueandreis@gmail.com>
2025-07-06 03:35:19 -0300

    Impletacao da criacao de conta com insercao em banco

--- api/bd/contas.py
+++ api/bd/contas.py
@@ -10,6 +10,9 @@ class Conta:
         self.foto_perfil = foto_perfil
         self.banner = banner
         self.sobre = sobre
+        
+    def set_id(self,novo_id):
+        self.id = novo_id
     
     def to_dict(self):      
         return {
@@ -26,6 +29,7 @@ class Perfil_Pessoal(Conta):
     def __init__(self, id: int, name: str, email: str, password: str, titulo: str, **kwargs):
         super().__init__(id=id,name=name,email=email, password=password, **kwargs)
         self.titulo = titulo
+        self.tipo = "pessoal"
     
     def to_dict(self):
         data = super().to_dict()
@@ -35,11 +39,13 @@ class Perfil_Pessoal(Conta):
         
 class Perfil_Empresarial(Conta):
     
-    def __init__(self, id: int, name: str, email: str, password: str, nome_fantasia: str, localizacao: str, setor: str, **kwargs):
+    def __init__(self, id: int, name: str, email: str, password: str, nome_fantasia: str, localizacao: str, setor: str, cod_institucional: int = None, **kwargs):
         super().__init__(id,name, email, password, **kwargs)
         self.nome_fantasia = nome_fantasia  
         self.localizacao = localizacao  
         self.setor = setor
+        self.cod_instucional = cod_institucional
+        self.tipo = "empresarial"
     def to_dict(self):
         data = super().to_dict()
         data['nomeFantasia'] = self.nome_fantasia
@@ -54,38 +60,63 @@ class Gerenciamento_Contas:
     def __init__(self,conexao):
         self.conexao = conexao
     
-    def generete_id():
-        return 0
-    
     def criar_conta(self, data: dict):
         
         nova_conta = self.gerar_conta(data)
         
         if nova_conta is not None:
             try:
-                nova_conta = self._insert_conta_banco(nova_conta)  
+                nova_conta = self.insert_conta_banco(nova_conta)  
                 self.conexao.commit()
             except (Exception, psycopg2.DatabaseError) as e:
+                self.conexao.rollback()
                 print(f"Falha ao inserir conta no banco de dados: {e}")
     
     def insert_conta_banco(self, conta):
-        # Em construção
+        
         with self.conexao.cursor() as cursor:
-            cursor.execute(f"INSERT INTO contas (Email, Nome, Senha, Foto_Perfil, Banner, Sobre) VALUES ()")
+            
+            cursor.execute("""
+                INSERT INTO contas (Email, Nome, Senha, Foto_Perfil, Banner, Sobre) 
+                VALUES (%s, %s, %s, %s, %s, %s)
+                RETURNING id;
+            """, (
+                conta.email, conta.name, conta.password,
+                conta.foto_perfil, conta.banner, conta.sobre
+            ))
+            
+            conta_id = cursor.fetchone()[0]
+            conta.set_id(conta_id)
+            
+            if conta.tipo == "pessoal":
+                
+                cursor.execute("""
+                    INSERT INTO perfil_p (titulo, cid)
+                    VALUES (%s, %s)
+                """, (conta.titulo, conta.id))
+                
+            elif conta.tipo == "empresarial": 
+                
+                cursor.execute("""
+                    INSERT INTO perfil_emp (CId, NomeFantasia, Localizacao, ID_setor, Cod_institucional)
+                    VALUES (%s, %s, %s, %s, %s)
+                """, (
+                    conta.id, conta.nome_fantasia, conta.localizacao,
+                    conta.setor, conta.cod_institucional
+                ))
+                
         
-        
-        return 0
             
     
     def gerar_conta(self, data: dict):
         
-        tipo_conta = data['type']
+        tipo_conta = data['tipo']
         nova_conta = None
         
         try:
             if tipo_conta == "pessoal":
                 nova_conta = Perfil_Pessoal(
-                    id=self.generate_id(),
+                    id = 0, # Id sera gerado inicialmente pelo banco
                     name=data['name'],
                     email=data['email'],
                     password=data['password'],
@@ -97,7 +128,7 @@ class Gerenciamento_Contas:
             
             elif tipo_conta == "empresarial":
                 nova_conta = Perfil_Empresarial(
-                    id=self.generate_id(),
+                    id = 0, # Id sera gerado inicialmente pelo banco
                     name=data['name'],
                     email=data['email'],
                     password=data['password'],
@@ -106,10 +137,12 @@ class Gerenciamento_Contas:
                     setor=data['setor'],
                     foto_perfil=data.get('foto_perfil'),
                     banner=data.get('banner'),
-                    sobre=data.get('sobre')
+                    sobre=data.get('sobre'),
+                    cod_institucional=data.get('cod_institucional')
                 )
             else:
                 return (None)
             return (nova_conta)
         except KeyError as e:
+            print(f"Campo ausente: {e}")
             return (None)
