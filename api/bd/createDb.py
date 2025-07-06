374a4096fee21f910a1976cd9ae77e37a38c1539
IgorHAndreis <igorhenriqueandreis@gmail.com>
2025-07-05 21:09:01 -0300

    Adicao de classes de conta e subclasses relacionas

--- api/bd/createDb.py
+++ api/bd/createDb.py
@@ -6,7 +6,7 @@ def teste():
 
 def connect_db():
     # Connect to your postgres DB
-    conn = psycopg2.connect("host=localhost dbname=Linkado user=postgres password=password")
+    conn = psycopg2.connect("host=localhost dbname=Linkado user=aplicacao password=senha123")
 
 # Open a cursor to perform database operations
     cur = conn.cursor()
